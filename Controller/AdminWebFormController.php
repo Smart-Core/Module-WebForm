94ab7ec88f23a7b8711a09f40bcb836daddecf0f
Smart-Core <artem.digi@gmail.com>
2015-08-25 01:51:15 +0600

    fix backurl

--- Controller/AdminWebFormController.php
+++ Controller/AdminWebFormController.php
@@ -279,9 +279,7 @@ class AdminWebFormController extends Controller
     {
         foreach ($this->get('cms.node')->findByModule('WebForm') as $node) {
             if ($node->getParam('webform_id') === (int) $webForm->getId()) {
-                return $this->generateUrl('smart_module.web_form.index', [
-                    '_folderPath' => $this->get('cms.folder')->getUri($node),
-                ]);
+                return $this->get('cms.folder')->getUri($node);
             }
         }
 
